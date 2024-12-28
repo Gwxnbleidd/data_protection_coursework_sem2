f7f5cc7236ccc7bab6c620c0126a9347def8aeb1
Gwxnbleidd <dima_russla@mail.ru>
2024-12-28 22:50:54 +0300

    переписано на бинарные файлы

--- core/electronic_signature.py
+++ core/electronic_signature.py
@@ -1,22 +1,21 @@
 import pathlib
+import pickle
 from dataclasses import dataclass
 
 from core import my_rsa
 from .file_info import FileInfo
 
-
-SPECIAL_DELIMETER = "special_delimeter"
+SPECIAL_DELIMETER = b"special_delimeter"
 
 @dataclass
 class VerifyStatus:
-    status: bool 
-    user_name: str 
+    status: bool
+    user_name: str
 
 class ElectronicSignature:
-    def __init__(self, key_size = None):
+    def __init__(self, key_size=None):
         if key_size:
             self.public_key, self.private_key = my_rsa.generate_keypair(key_size // 2, key_size // 2, key_size)
-
 
     def sign(self, doc_name: pathlib.Path):
         file_info = FileInfo.collect(doc_name)
@@ -24,22 +23,24 @@ class ElectronicSignature:
         self.signed_val = my_rsa.sign(self.private_key, file_info_json)
 
     def save_signature(self, user_name: str, signature_file_name: pathlib.Path):
-        with open(signature_file_name, "w") as f:
-            f.write(user_name + SPECIAL_DELIMETER)
-            f.write(self.signed_val)
+        with open(signature_file_name, "wb") as f:
+            f.write(user_name.encode() + SPECIAL_DELIMETER)
+            f.write(self.signed_val.encode())
 
     def verify(self, doc_name: pathlib.Path, signature_file_name: pathlib.Path, public_key_file: pathlib.Path) -> VerifyStatus:
         file_info = FileInfo.collect(doc_name)
         file_info_json = file_info.to_json()
-        with open(signature_file_name, "r") as f:
+        with open(signature_file_name, "rb") as f:
             data = f.read()
         user_name, signature = data.split(SPECIAL_DELIMETER)
+        user_name = user_name.decode()
+        signature = signature.decode()
         self._load_public_key(public_key_file)
         if my_rsa.verify(self.public_key, signature, file_info_json):
             return VerifyStatus(True, user_name)
         else:
             return VerifyStatus(False, user_name)
-        
+
     def save_keys(self, folder_name: pathlib.Path):
         folder_name.mkdir(parents=True, exist_ok=True)
         self._save_public_key(folder_name / "public_key")
@@ -50,19 +51,17 @@ class ElectronicSignature:
         return self._load_private_key(folder_name / "private_key")
 
     def _save_public_key(self, public_key_path: pathlib.Path):
-        with open(public_key_path, "w") as f:
-            f.write(','.join([str(el) for el in self.public_key]))
-    
+        with open(public_key_path, "wb") as f:
+            pickle.dump(self.public_key, f)
+
     def _save_private_key(self, private_key_path: pathlib.Path):
-        with open(private_key_path, "w") as f:
-            f.write(','.join([str(el) for el in self.private_key]))
+        with open(private_key_path, "wb") as f:
+            pickle.dump(self.private_key, f)
 
     def _load_public_key(self, public_key_file_name: pathlib.Path):
-        with open(public_key_file_name, "r") as f:
-            data = f.read()
-        self.public_key = tuple(map(int, data.split(',')))
-    
+        with open(public_key_file_name, "rb") as f:
+            self.public_key = pickle.load(f)
+
     def _load_private_key(self, private_key_file_name: pathlib.Path):
-        with open(private_key_file_name, "r") as f:
-            data = f.read()
-        self.private_key = tuple(map(int, data.split(',')))
+        with open(private_key_file_name, "rb") as f:
+            self.private_key = pickle.load(f)
