b081fbddc251439c234fea991f049dce1024d5cd
Gwxnbleidd <dima_russla@mail.ru>
2025-04-27 18:00:32 +0300

    Сделал модуль создания и проверки электронной подписи и сделал вспомогательный (математический) модуль

---
+++ core/fiat_shamir_signature.py
@@ -0,0 +1,94 @@
+from core.utils import *
+
+
+def generate_keypair(max_n: int, t: int = 8):
+    """
+    Функция генерирует закрытый и открытый ключи
+
+    :param max_n: Максимальное возможное значение n
+    :param t: Число значений в списках v и s
+    :return: Открытый ключ, закрытый ключ
+    """
+    if max_n < 512:
+        raise Exception('Число должно быть не меньше 512')
+
+    p, q = generate_p_q(512, max_n)
+    n = p * q
+
+    s = [generate_coprime(n) for i in range(t)]
+    v = [pow(s_val, -2, n) for s_val in s]
+
+    return (n, v), (n, s)
+
+
+def sign(private_key: tuple, message: str):
+    """
+    Функция подписывает сообщение
+
+    :param private_key: Приватный ключ и n
+    :param message: Сообщение, которое необходимо подписать
+    :return: Подпись в формате b_ij и y_i
+    """
+    n, s = private_key
+    t = len(s)
+    k = random.randint(7, t)
+
+    r = [random.randint(1, n) for _ in range(t)]
+    x = [pow(r_val, 2, n) for r_val in r]
+
+    hash_val = get_hash_fiat_shamir(message.encode('utf-8'), x, n)
+    binary_hash = ''.join([format(byte, '08b') for byte in hash_val])
+
+    b = [[int(binary_hash[i * k + j]) for j in range(k)] for i in range(t)]
+
+    y = []
+    for i in range(t):
+        y_val = r[i]
+        for j in range(k):
+            if b[i][j]:
+                y_val = (y_val * s[j]) % n
+        y.append(y_val)
+
+    return b, y
+
+
+def verify(public_key: tuple, sign: tuple, message: str) -> bool:
+    """
+    Функция проверяет подпись по протоколу Фиата-Шамира
+
+    :param public_key: Публичный ключ и n
+    :param sign: Подпись
+    :message sign: Сообщение, которое нужно проверить
+    :return: True - все верно, False - Нет
+    """
+    n, v = public_key
+    b, y = sign
+    t, k = len(v), len(b[0])
+
+    z = []
+    for i in range(t):
+        z_val = pow(y[i], 2, n)
+        for j in range(k):
+            if b[i][j]:
+                z_val = (z_val * v[j]) % n
+        z.append(z_val)
+
+    # Вычисляем хеш от (message, z) и получаем b_new
+    hash_val = get_hash_fiat_shamir(message.encode('utf-8'), z, n)
+    binary_hash = ''.join([format(byte, '08b') for byte in hash_val])
+
+    # Формируем новую битовую матрицу
+    b_new = [[int(binary_hash[i * k + j]) for j in range(k)] for i in range(t)]
+
+    return b == b_new
+
+
+if __name__ == '__main__':
+    # Пример использования:
+    public_key, private_key = generate_keypair(1024)
+    message = "Hello, Fiat-Shamir!"
+    b, y = sign(private_key, message)
+    is_valid = verify(public_key, (b, y), message)
+    print(is_valid)
+
+
--- core/utils.py
+++ core/utils.py
@@ -1,47 +1,103 @@
-import math as m
+import math
 import random
+from hashlib import sha256 as hash_func
 
 
-def gen_prime(bit_length=1024):
-    while True:
-        num = random.getrandbits(bit_length)
-        if is_prime(num):
-            return num
+def is_prime(num: int) -> bool:
+    """
+    Функция проверяет число на простоту
 
-
-def is_prime(n, k=5):
-    if n == 2 or n == 3:
-        return True
-    if n <= 1 or n % 2 == 0:
+    :param num: Число
+    :return: True - простое, False - составное
+    """
+    if num <= 1:
         return False
-
-    r, s = 0, n - 1
-    while s % 2 == 0:
-        r += 1
-        s //= 2
-
-    for _ in range(k):
-        a = random.randint(2, n - 1)
-        x = pow(a, s, n)
-        if x == 1 or x == n - 1:
-            continue
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                break
-        else:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
 
-def inverse(a: int, m: int) -> int:
-    m0, x0, x1 = m, 0, 1
-    while a > 1:
-        q = a // m
-        m, a = a % m, m
-        x0, x1 = x1 - q * x0, x0
-    return x1 + m0 if x1 < 0 else x1
+def generate_prime(min_value: int, max_value: int) -> int | None:
+    """
+    Функция генерирует простое число на заданном отрезке или None, если не нашлось число на отрезке
+
+    :param min_value: Нижняя граница отрезка
+    :param max_value: Верхняя граница отрезка
+    :return: Простое число на заданном отрезке
+    """
+    for i in range(100):
+        num = random.randint(min_value, max_value)
+        if is_prime(num):
+            return num
+    return None
 
 
-def gcd(a: int, b: int) -> int:
-    return a if b == 0 else m.gcd(a, a % b)
+def generate_p_q(min_n: int, max_n: int) -> tuple[int, int] | None:
+    """
+    Функция генерирует два простых числа, в произведении дающих от min_n (512) до max_n (1024)
+
+    :param min_n: Минимальное значение произведения простых чисел
+    :param max_n: Максимальное значение произведения простых чисел
+    :return: q, p - простые числа
+    """
+    attempts, max_attempts = 0, 1000
+
+    while attempts < max_attempts:
+        p = generate_prime(2, max_n // 2 + 1)  # Генерируем p так, чтобы q было не меньше 2
+
+        if not p:
+            attempts += 1
+            continue
+
+        max_q = max_n // p
+        q = generate_prime(2, max_q)
+
+        if not q:
+            attempts += 1
+            continue
+
+        if min_n <= p * q <= max_n:
+            return q, p
+        else:
+            attempts += 1
+            continue
+
+    return generate_p_q(min_n, max_n)
+
+
+def generate_coprime(n: int) -> int:
+    """
+    Функция генерирует взаимно простое с n число
+
+    :param n: Число, для которого нужно найти взаимно простое
+    :return: взаимно простое число
+    """
+    if n == 1:
+        return 1
+
+    while True:
+        m = random.randint(2, n - 1)  # Генерируем число от 2 до n - 1
+        if math.gcd(n, m) == 1:  # Проверяем, взаимно ли просто
+            return m
+
+
+def get_hash_fiat_shamir(message: bytes, x: list[int], n: int) -> bytes:
+    """
+    Функция получает хэш (по алгоритму sha256) сообщения и всех Х чисел
+
+    :param message: Сообщение, которое нужно подписать
+    :param x: x_i = (r_i ** 2) % n, где r_i - случайное число от 1
+    :param n: произведение двух простых чисел p * q
+    :return: хэш
+    """
+    data = message
+
+    for x_val in x:
+        data += x_val.to_bytes((n.bit_length() + 7) // 8, byteorder='big')
+
+    return hash_func(data).digest()
+
+
+if __name__ == '__main__':
+    print(generate_p_q(512, 9128))
